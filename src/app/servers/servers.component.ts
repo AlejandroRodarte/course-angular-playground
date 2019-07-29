471e46c5cca0d70f76c7c53ab032c9082bad570d
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2019-07-28 22:35:43 -0600

    Lecture 21: Fully Understanding the Component Selector

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,10 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+// servers @component definition
+// the supported selectors are by tag name, tag attribute and class name
 @Component({
+  // selector: 'app-servers',
+  // selector: '[app-servers]',
   selector: 'app-servers',
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
+
 export class ServersComponent implements OnInit {
 
   constructor() { }
