fce1ecedf40e2ce83dd87d6ee65c175a0d1fa1ab
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2019-07-28 20:30:45 -0600

    Lecture 6 comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
+// define a custom <app-root> html tag for our index.html file
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+// export the component with a name property
 export class AppComponent {
   name = 'Alejandro';
 }
