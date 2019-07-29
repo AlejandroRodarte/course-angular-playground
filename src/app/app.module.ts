670e1806f293990035ead36595e917553db1f5ca
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2019-07-28 21:59:47 -0600

    Lecture 16: Understanding the Role of AppModule and Component Declaration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,24 @@
 // required imports for the app go here
 
+// default modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 // FormsModule enables directives to set model attributes
 import { FormsModule } from '@angular/forms';
 
+// import root and custom components
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
 
-// here go the components/modules we will implement on our app 
+// here go the components/modules we will implement on our app
+// declarations : declare all components 
+// imports : declare required external modules
+// bootstrap : component to load when kickstarting app
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule,
