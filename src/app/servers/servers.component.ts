60bccae35924a74ddfa3cbbf89c6b7dc6f75cc51
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2019-07-29 14:39:26 -0600

    Lecture 25: Property Binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -3,18 +3,27 @@ import { Component, OnInit } from '@angular/core';
 // servers @component definition
 // the supported selectors are by tag name, tag attribute and class name
 @Component({
-  // selector: 'app-servers',
-  // selector: '[app-servers]',
-  selector: 'app-servers',
-  templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+    // selector: 'app-servers',
+    // selector: '[app-servers]',
+    selector: 'app-servers',
+    templateUrl: './servers.component.html',
+    styleUrls: ['./servers.component.css']
 })
 
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+    // new boolean property: will be bind to the 'disabled' property of the button element
+    allowNewServer: boolean = false;
 
-  ngOnInit() {
-  }
+    // constructor: after 2 seconds, set the boolean field to true
+    constructor() { 
+        setTimeout(() => {
+            this.allowNewServer = true;
+        }, 2000)
+    }
+
+    ngOnInit() {
+
+    }
 
 }
