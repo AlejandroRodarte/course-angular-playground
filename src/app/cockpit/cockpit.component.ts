d0cb74e1c3d8e19d9c5b9d1a313c3a007be23d13
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2019-07-30 21:38:52 -0600

    Lecture 72: Using Local References in Templates

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -19,7 +19,6 @@ export class CockpitComponent implements OnInit {
 	blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
 
 	// server name and server content
-  	newServerName = '';
 	newServerContent = '';
 
 	constructor() { }
@@ -33,9 +32,11 @@ export class CockpitComponent implements OnInit {
 	// when the add server button from the cockpit.component.html is clicked, it will
 	// use the event emitter to call the emit() method and transmit some data that will be outputted
 	// to some parent component
-  	onAddServer() {
+
+	// using local reference of an html input element to access the input's value that holds the name
+  	onAddServer(nameInput: HTMLInputElement) {
 		this.serverCreated.emit({
-			serverName: this.newServerName,
+			serverName: nameInput.value,
 			serverContent: this.newServerContent
 		});
 	}
@@ -46,9 +47,11 @@ export class CockpitComponent implements OnInit {
 	// when the add blueprint button from the cockpit.component.html is clicked, it will
 	// use the event emitter to call the emit() method and transmit some data that will be outputted
 	// to some parent component
-	onAddBlueprint() {
+
+	// using local reference of an html input element to access the input's value that holds the name
+	onAddBlueprint(nameInput: HTMLInputElement) {
 		this.blueprintCreated.emit({
-			serverName: this.newServerName,
+			serverName: nameInput.value,
 			serverContent: this.newServerContent
 		});
 	}
