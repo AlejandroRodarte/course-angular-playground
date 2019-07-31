f176b2971bc96daf7691e3ecfe2c51bd451cc0fb
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2019-07-31 15:24:22 -0600

    Lecture 84: Adding navigation with Event Binding and nglf (my solution)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,26 @@ import { Component } from '@angular/core';
 
 // this component will go inside an <app-root> html tag found on the index.html page
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 
 // component class definition
 export class AppComponent {
+	
+	// property that holds the current section
+	section: string;
+
+	// display the recipes section by default
+	constructor() {
+		this.section = 'recipes';
+	}
+
+	// event handler triggered when data is emitted from the header
+	// set the new section to display
+	setSection(data: { section: string }): void {
+		this.section = data.section;
+	}
+
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
     selector: 'app-header',
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
     styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+
+    // event emitter to send the section we want to display
+    @Output()
+    sectionToDisplay = new EventEmitter<{ section: string }>();
+
+    // when clicking 'recipes' on the header, send an object with the section to display (recipes section)
+    onRecipesClick(): void {
+        this.sectionToDisplay.emit({ section: 'recipes' });
+    }
+
+    // when clicking 'shopping list' on the header, send an object with the section to display (shopping list section)
+    onShoppingListClick(): void {
+        this.sectionToDisplay.emit({ section: 'shoppingList' });
+    }
+
 
 }
