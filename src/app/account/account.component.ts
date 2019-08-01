b7e167d36060eb9bdfd824620c8bac78ad1a2f64
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2019-08-01 16:50:27 -0600

    Lecture 106: Injecting the Logging Service into Components

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,10 +1,13 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { LoggingService } from '../logging.service';
 
 // account component
+// requiring to inject LoggingService to constructor
 @Component({
     selector: 'app-account',
     templateUrl: './account.component.html',
-    styleUrls: ['./account.component.css']
+    styleUrls: ['./account.component.css'],
+    providers: [LoggingService]
 })
 export class AccountComponent {
 
@@ -15,6 +18,11 @@ export class AccountComponent {
     // receive the id of the account (index of the *ngFor loop)
     @Input() 
     id: number;
+
+    // constructor: required to inject to this component the logging service
+    constructor(private loggingService: LoggingService) {
+
+    }
 
     // event emitter: root component will listen for this statusChanged emitter
     @Output() 
@@ -29,7 +37,8 @@ export class AccountComponent {
             newStatus: status}
         );
 
-        console.log('A server status changed, new status: ' + status);
+        // using the injected service to log data
+        this.loggingService.logStatusChange(status);
 
     }
 
--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,16 +1,26 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { LoggingService } from '../logging.service';
 
 // new account component
+// providers property: services that should be injected into this component (LoggingService)
+// we inform Angular that we desire to inject in the constructor an instance of the LoggingService class
 @Component({
 	selector: 'app-new-account',
 	templateUrl: './new-account.component.html',
-	styleUrls: ['./new-account.component.css']
+	styleUrls: ['./new-account.component.css'],
+	providers: [LoggingService]
 })
 export class NewAccountComponent {
 
 	// accountAdded event emitter: listened by the root component
 	@Output() 
 	accountAdded = new EventEmitter<{name: string, status: string}>();
+
+	// dependency injection: inject a LoggingService instance automatically into the constructor
+	// Angular handles in the background the creation of the new instance and injecting it through here
+	constructor(private loggingService: LoggingService) {
+
+	}
 
 	// onCreateAccount() submission handler: receive account name and status
 	onCreateAccount(accountName: string, accountStatus: string) {
@@ -21,7 +31,8 @@ export class NewAccountComponent {
 			status: accountStatus
 		});
 
-		console.log('A server status changed, new status: ' + accountStatus);
+		// using the injected logging service instance to access its method and log message to console
+		this.loggingService.logStatusChange(accountStatus);
 
 	}
 
