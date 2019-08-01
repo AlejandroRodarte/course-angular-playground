44d49eb945277a52683717152122523524ea3e6a
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2019-07-31 20:00:16 -0600

    Lecture 90 and 91: ngFor and ngIf structural directives; ngClass and ngStyle attribute directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,23 @@
-// component parent node selector goes here
-// component class definition goes here
-
 import { Component } from '@angular/core';
 
-// this component will go inside an <app-root> html tag found on the index.html page
+// app component
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
-
-// component class definition
 export class AppComponent {
+
+	// array of numbers
+	numbers = [1, 2, 3, 4, 5];
+
+	// odd numbers array
+	oddNumbers = [1, 3, 5];
+
+	// even numbers array
+	evenNumbers = [2, 4];
 	
-	// property that holds the current feature
-	feature: string = 'recipes';
-
-	// event handler triggered when data is emitted from the header
-	// set the new feature to display
-	setFeature(feature: string): void {
-		this.feature = feature;
-	}
-
+	// toggle for only odd numbers
+	onlyOdd = false;
+	
 }
