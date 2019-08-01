0eb91984fa0745849bed6daf21488a5d25b159f6
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2019-07-31 21:50:53 -0600

    Section 8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,25 @@
+// component parent node selector goes here
+// component class definition goes here
+
 import { Component } from '@angular/core';
 
-// app component
+// this component will go inside an <app-root> html tag found on the index.html page
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
+
+// component class definition
 export class AppComponent {
-
-	// array of numbers
-	numbers = [1, 2, 3, 4, 5];
-
-	// odd numbers array
-	oddNumbers = [1, 3, 5];
-
-	// even numbers array
-	evenNumbers = [2, 4];
 	
-	// toggle for only odd numbers
-	onlyOdd = false;
+	// property that holds the current feature
+	feature: string = 'recipes';
 
-	value = 5;
-	
+	// event handler triggered when data is emitted from the header
+	// set the new feature to display
+	setFeature(feature: string): void {
+		this.feature = feature;
+	}
+
 }
