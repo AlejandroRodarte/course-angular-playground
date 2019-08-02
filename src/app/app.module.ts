2b6c7819417ce094bd3ffde935bc256ad2ee6e14
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2019-08-02 13:40:02 -0600

    Lecture 125: Setting up and Loading Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
@@ -12,7 +13,32 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 
+// app routes are defined in the app.module.ts file
+const appRoutes: Routes = [
+
+	// localhost:4200/: load HomeComponent
+	{
+		path: '',
+		component: HomeComponent
+	},
+
+	// localhost:4200/users: load UsersComponent
+	{
+		path: 'users',
+		component: UsersComponent
+	},
+
+	// localhost:4200/servers: load ServersComponent
+	{
+		path: 'servers',
+		component: ServersComponent
+	}
+
+];
+
 // serversService defines a global service (singleton)
+// using the RouterModule to use the Angular router
+// method forRoot() allows us to register or constant that defines the application routes
 @NgModule({
   declarations: [
     AppComponent,
@@ -25,7 +51,8 @@ import { ServersService } from './servers/servers.service';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
+	FormsModule,
+	RouterModule.forRoot(appRoutes)
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
