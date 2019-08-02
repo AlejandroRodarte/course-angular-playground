3846682f875247a598917126c47fcb2a02493f20
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2019-08-02 16:37:12 -0600

    Lecture 129: Navigating Programatically

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 // home component
 @Component({
@@ -8,12 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-	constructor() { 
+	// we can inject the application global router
+	constructor(private router: Router) { 
 
 	}
 
 	ngOnInit() {
 		
+	}
+
+	onLoadServers(): void {
+
+		// ...perform some complicated algorithm
+
+		// access the router an navigate to localhost:4200/servers
+		this.router.navigate(['/servers']);
+
 	}
 
 }
