68efe144837a8e95433efe080a8dfb39d4154c4b
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2019-08-02 17:41:48 -0600

    Lecture 136: Retrieving Query Parameters and Fragments

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
+import { ActivatedRoute } from '@angular/router';
 
 // edit server component
 @Component({
@@ -18,16 +19,29 @@ export class EditServerComponent implements OnInit {
 	serverStatus = '';
 
 	// inject the servers global service
-	constructor(private serversService: ServersService) { 
+	constructor(private serversService: ServersService,
+				private route: ActivatedRoute) { 
 		
 	}
 
 	// on initialization, render the first server by getting it from 
 	// the getter of the servers service
 	ngOnInit() {
+		
+		// first way of fetching query parameters and fragmets
+		// access the snapshost url: will only load once
+		console.log(this.route.snapshot.queryParams);
+		console.log(this.route.snapshot.fragment);
+
+		// if we desire to react to url changes after the component has been loaded
+		// we need to subscribe to the queryParams and fragment observables
+		this.route.queryParams.subscribe();
+		this.route.fragment.subscribe();
+
 		this.server = this.serversService.getServer(1);
 		this.serverName = this.server.name;
 		this.serverStatus = this.server.status;
+
 	}
 
 	// onUpdateServer handler: use the server service to update the server with the given id
