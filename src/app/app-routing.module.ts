52aff74a9823b1218299469d221b4fba6bf3f065
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2019-08-03 15:34:45 -0600

    Lecture 143: Outsourcing the Route Configuration

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,88 @@
+import { Routes, RouterModule, Router } from '@angular/router';
+
+import { NgModule } from '@angular/core';
+import { HomeComponent } from './home/home.component';
+import { UsersComponent } from './users/users.component';
+import { UserComponent } from './users/user/user.component';
+import { ServersComponent } from './servers/servers.component';
+import { ServerComponent } from './servers/server/server.component';
+import { EditServerComponent } from './servers/edit-server/edit-server.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+// app routes are defined in the app.module.ts file
+const appRoutes: Routes = [
+
+	// localhost:4200/: load HomeComponent
+	{
+		path: '',
+		component: HomeComponent
+	},
+
+	// localhost:4200/users: load UsersComponent
+	{
+		path: 'users',
+		component: UsersComponent,
+		children: [
+
+			// load a single user component by id
+			// the ':' informs Angular that this a dynamic part of the path (the user id)
+			// add a name dynamic piece of data too
+			{
+				path: ':id/:name',
+				component: UserComponent
+			}
+
+		]
+	},
+
+	// localhost:4200/servers: load ServersComponent
+	{
+		path: 'servers',
+		component: ServersComponent,
+		children: [
+
+			// localhost:4200/servers/id
+			{
+				path: ':id',
+				component: ServerComponent,
+			},
+
+			// localhost:4200/id/edit
+			{
+				path: ':id/edit',
+				component: EditServerComponent
+			}
+
+		]
+	},
+
+	// localhost:4200/not-found
+	// component to load whenever user attempts to access unknown route
+	{
+		path: 'not-found',
+		component: PageNotFoundComponent
+	},
+
+	// ** wildcard: covers all paths unknown to us
+	// will not load a component but redirect to /not-found, which loads the PageNotFoundComponent
+	// make sure to place this at the very botton: paths are parsed from top to bottom!
+	{
+		path: '**',
+		redirectTo: '/not-found'
+	}
+
+];
+
+// app routing module
+// import the router module and apply forRoot() to register the routes
+@NgModule({
+    imports: [
+        RouterModule.forRoot(appRoutes)
+    ],
+    exports: [
+        RouterModule
+    ]
+})
+export class AppRoutingModule {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { Routes, RouterModule } from '@angular/router';
-
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -13,70 +11,7 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-
-// app routes are defined in the app.module.ts file
-const appRoutes: Routes = [
-
-	// localhost:4200/: load HomeComponent
-	{
-		path: '',
-		component: HomeComponent
-	},
-
-	// localhost:4200/users: load UsersComponent
-	{
-		path: 'users',
-		component: UsersComponent,
-		children: [
-
-			// load a single user component by id
-			// the ':' informs Angular that this a dynamic part of the path (the user id)
-			// add a name dynamic piece of data too
-			{
-				path: ':id/:name',
-				component: UserComponent
-			}
-
-		]
-	},
-
-	// localhost:4200/servers: load ServersComponent
-	{
-		path: 'servers',
-		component: ServersComponent,
-		children: [
-
-			// localhost:4200/servers/id
-			{
-				path: ':id',
-				component: ServerComponent,
-			},
-
-			// localhost:4200/id/edit
-			{
-				path: ':id/edit',
-				component: EditServerComponent
-			}
-
-		]
-	},
-
-	// localhost:4200/not-found
-	// component to load whenever user attempts to access unknown route
-	{
-		path: 'not-found',
-		component: PageNotFoundComponent
-	},
-
-	// ** wildcard: covers all paths unknown to us
-	// will not load a component but redirect to /not-found, which loads the PageNotFoundComponent
-	// make sure to place this at the very botton: paths are parsed from top to bottom!
-	{
-		path: '**',
-		redirectTo: '/not-found'
-	}
-
-];
+import { AppRoutingModule } from './app-routing.module';
 
 // serversService defines a global service (singleton)
 // using the RouterModule to use the Angular router
@@ -95,7 +30,7 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
 	FormsModule,
-	RouterModule.forRoot(appRoutes)
+	AppRoutingModule
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
