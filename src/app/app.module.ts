934d26e2b3c38572bdeae5fedc0331e78533fd2d
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2019-08-03 10:19:34 -0600

    Lecture 138: Setting up Child (Nested) Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,33 +25,39 @@ const appRoutes: Routes = [
 	// localhost:4200/users: load UsersComponent
 	{
 		path: 'users',
-		component: UsersComponent
+		component: UsersComponent,
+		children: [
+
+			// load a single user component by id
+			// the ':' informs Angular that this a dynamic part of the path (the user id)
+			// add a name dynamic piece of data too
+			{
+				path: ':id/:name',
+				component: UserComponent
+			}
+
+		]
 	},
 
 	// localhost:4200/servers: load ServersComponent
 	{
 		path: 'servers',
-		component: ServersComponent
-	},
+		component: ServersComponent,
+		children: [
 
-	// load a single user component by id
-	// the ':' informs Angular that this a dynamic part of the path (the user id)
-	// add a name dynamic piece of data too
-	{
-		path: 'users/:id/:name',
-		component: UserComponent
-	},
+			// localhost:4200/servers/id
+			{
+				path: ':id',
+				component: ServerComponent,
+			},
 
-	// localhost:4200/idNumber/edit
-	{
-		path: 'servers/:id/edit',
-		component: EditServerComponent
-	},
+			// localhost:4200/id/edit
+			{
+				path: ':id/edit',
+				component: EditServerComponent
+			}
 
-	// localhost:4200/servers/idNumber
-	{
-		path: 'servers/:id',
-		component: ServerComponent
+		]
 	}
 
 ];
