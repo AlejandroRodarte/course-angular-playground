7a0d3e33cfd8e015b88eff27777be34d52c32082
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2019-08-05 16:08:14 -0600

    Lecture 201: Reactive: Creating a Form in Code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,34 @@
-import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
 	// genders on radio buttons
 	genders = ['male', 'female'];
 	
 	// variable to store the form: must be of type FormGroup
 	signupForm: FormGroup;
+
+	ngOnInit(): void {
+
+		// controls go inside object literal as key/value pairs
+		// key: form control name
+		// value: a FormControl object
+
+		// FormControl arguments
+		// default control value
+		// on 'gender', we set the initial radio button value: 'male'
+		this.signupForm = new FormGroup({
+			'username': new FormControl(null),
+			'email': new FormControl(null),
+			'gender': new FormControl(this.genders[0])
+		});
+
+	}
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,14 +8,14 @@ import { AppComponent } from './app.component';
 // ReactiveFormsModule will allow us to build our form in the compoent and bind
 // it to the HTML form
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    ReactiveFormsModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		ReactiveFormsModule,
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
