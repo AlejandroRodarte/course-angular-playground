2baccf1180e4a035e0bf6a6e7216a2ff99897bd5
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2019-08-04 18:49:47 -0600

    Lecture 171: Building a Custom Observable

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 
 // import interval function since it is a way to create a new observable 
-import { interval, Subscription } from 'rxjs';
+import { interval, Subscription, Observable, Observer } from 'rxjs';
 
 @Component({
 	selector: 'app-home',
@@ -17,12 +17,39 @@ export class HomeComponent implements OnInit, OnDestroy {
 
 	}
 
-	// on initialization, subscribe to the interval() observer and print out the emitted
-	// count value
+	// on initialization
 	ngOnInit() {
-		this.firstObsSubscription = interval(1000).subscribe(count => {
-			console.log(count);
+		
+		// subscribe to the interval() observer and print out the emitted
+		// count value
+		// this.firstObsSubscription = interval(1000).subscribe(count => {
+		// 	console.log(count);
+		// })
+
+		// create a custom observable
+		// call Observable.create and pass in an arrow function
+		// rxjs will automatically inject the observer that listens to us
+		// this observer should be of type Observer and pass as a generic the type of data that
+		// this observer expects
+
+		// this arrow function simplt returns to the observer the value of the count variable each second
+
+		// next() is to send data, error() is to send error data, and complete() is to send completion data
+		const customIntervalObservable = Observable.create((observer: Observer<number>) => {
+
+			let count = 0;
+
+			setInterval(() => {
+				observer.next(count);
+				count++;
+			}, 1000);
+
+		});
+
+		this.firstObsSubscription = customIntervalObservable.subscribe((data: number) => {
+			console.log(data);
 		})
+
 	}
 
 	// upon leaving the component, unsubscribe from all subscriptions
