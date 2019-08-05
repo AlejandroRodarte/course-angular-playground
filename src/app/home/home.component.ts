2166b778bc234d53f2fc9bba2a9e721d8a3f4b2d
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2019-08-04 19:29:57 -0600

    Lecture 174: Understanding Operators

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,6 +3,9 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 // import interval function since it is a way to create a new observable 
 import { interval, Subscription, Observable, Observer } from 'rxjs';
 
+// operator method map
+import { map, filter } from 'rxjs/operators';
+
 @Component({
 	selector: 'app-home',
 	templateUrl: './home.component.html',
@@ -35,7 +38,7 @@ export class HomeComponent implements OnInit, OnDestroy {
 		// this arrow function simplt returns to the observer the value of the count variable each second
 
 		// next() is to send data, error() is to send error data, and complete() is to send completion data
-		const customIntervalObservable = Observable.create((observer: Observer<number>) => {
+		const customIntervalObservable: Observable<number> = Observable.create((observer: Observer<number>) => {
 
 			let count = 0;
 
@@ -45,12 +48,12 @@ export class HomeComponent implements OnInit, OnDestroy {
 				observer.next(count);
 
 				// emit completed notification with complete()
-				if (count === 2) {
+				if (count === 15) {
 					observer.complete();
 				}
 
 				// emit error data with error()
-				if (count > 3) {
+				if (count > 19) {
 					observer.error(new Error('Count is greater than 3. The apocalypsis is upon us.'))
 				}
 
@@ -64,14 +67,32 @@ export class HomeComponent implements OnInit, OnDestroy {
 		// 1. handler for observer.next()
 		// 2. handler for observer.error()
 		// 3. handler for observer.complete()
-		this.firstObsSubscription = customIntervalObservable.subscribe((data: number) => {
-			console.log(data);
-		}, (error: Error) => {
-			console.log(error);
-			alert(error.message);
-		}, () => {
-			console.log('task completed: doing some cleanup work...')
-		});
+
+		// every observable has a pipe() method, where we can insert some operator type, such as map()
+		// map() accepts a project function to transform each piece of emitted data
+
+		// workflow:
+		// 1. recieve raw data (number) in the pipe()
+		// 2. inside the pipe(), apply the map() function to alter the information and format it to a string
+		// 3. subscribe() to the piped data (string) and log it
+
+		// note: pipe() accepts an unlimited amount of operators, so more operators after map() can exist
+
+		// filter() accepts a predicate which is a method that returns true or false with criteria to filter out some data
+		this.firstObsSubscription = customIntervalObservable
+			.pipe(filter((data: number): boolean => {
+				return data % 2 === 0;
+			}), map((data: number): string => {
+				return 'Round: ' + (data + 1);
+			}))
+			.subscribe((data: string) => {
+				console.log(data);
+			}, (error: Error) => {
+				console.log(error);
+				alert(error.message);
+			}, () => {
+				console.log('task completed: doing some cleanup work...')
+			});
 
 	}
 
