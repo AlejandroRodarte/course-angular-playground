2b382b0ecff390ab092373228df2f6fa45b364de
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2019-08-04 18:31:41 -0600

    Lecture 169: Understanding Angular Observables

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,18 +4,25 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { UserComponent } from './user/user.component';
 
+// paths to root to load HomeComponent and user/id to load UserComponent
 const routes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'user/:id', component: UserComponent}
+	{
+		path: '', 
+		component: HomeComponent
+	},
+	{
+		path: 'user/:id', 
+		component: UserComponent
+	}
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes)
-  ],
-  exports: [
-    RouterModule
-  ]
+	imports: [
+		RouterModule.forRoot(routes)
+	],
+	exports: [
+		RouterModule
+	]
 })
 export class AppRoutingModule {
 
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -2,19 +2,24 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
-  selector: 'app-user',
-  templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+	selector: 'app-user',
+	templateUrl: './user.component.html',
+	styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
-  id: number;
 
-  constructor(private route: ActivatedRoute) {
-  }
+	// user id
+	id: number;
 
-  ngOnInit() {
-    this.route.params.subscribe((params: Params) => {
-      this.id = +params.id;
-    });
-  }
+	// load current route
+	constructor(private route: ActivatedRoute) {
+
+	}
+
+	// subscribe to route params, extract id and assign to field value
+	ngOnInit() {
+		this.route.params.subscribe((params: Params) => {
+			this.id = +params.id;
+		});
+	}
 }
