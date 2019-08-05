1622a8ba5cfcc9e74f5c90da529c67b8a6414460
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2019-08-04 18:41:12 -0600

    Lecture 170: Getting closer to the core of observables

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,18 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+// import interval function since it is a way to create a new observable 
+import { interval, Subscription } from 'rxjs';
 
 @Component({
 	selector: 'app-home',
 	templateUrl: './home.component.html',
 	styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+
+	// store subscription
+	private firstObsSubscription: Subscription;
 
 	constructor() { 
 
 	}
 
+	// on initialization, subscribe to the interval() observer and print out the emitted
+	// count value
 	ngOnInit() {
-		
+		this.firstObsSubscription = interval(1000).subscribe(count => {
+			console.log(count);
+		})
+	}
+
+	// upon leaving the component, unsubscribe from all subscriptions
+	ngOnDestroy() {
+		this.firstObsSubscription.unsubscribe();
 	}
 
 }
