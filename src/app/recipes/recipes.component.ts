c6a73b1b68cb2260a021bb4780fe90283ff75078
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2019-08-05 23:40:39 -0600

    Section 16

---
+++ src/app/recipes/recipes.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { RecipeService } from './recipe.service';
+
+// inject to this parent component the recipe service
+// RecipeListComponent, RecipeDetailComponent and RecipeItemComponent will share also this singleton instance
+@Component({
+	selector: 'app-recipes',
+	templateUrl: './recipes.component.html',
+	styleUrls: ['./recipes.component.css'],
+	providers: [RecipeService]
+})
+export class RecipesComponent implements OnInit {
+
+	constructor() {
+
+	}
+
+	ngOnInit() {
+
+	}
+
+}
