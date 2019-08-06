24a945c1e3a4a6c8f0b496cbef751f7ad46d3158
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2019-08-05 20:34:26 -0600

    Lecture 208: Arrays of Form Controls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormArray, AbstractControl } from '@angular/forms';
 
 @Component({
 	selector: 'app-root',
@@ -33,6 +33,8 @@ export class AppComponent implements OnInit {
 
 		// how to access username with the get() method: get(userData.username)
 		// how to access email with the get() method: get(userData.email)
+
+		// hobbies: a form array (array of text inputs)
 		this.signupForm = new FormGroup({
 
 			'userData': new FormGroup({
@@ -40,7 +42,9 @@ export class AppComponent implements OnInit {
 				'email': new FormControl(null, [Validators.required, Validators.email])
 			}),
 
-			'gender': new FormControl(this.genders[0])
+			'gender': new FormControl(this.genders[0]),
+
+			'hobbies': new FormArray([])
 
 		});
 
@@ -49,6 +53,25 @@ export class AppComponent implements OnInit {
 	// on submission, log signup form
 	onSubmit(): void {
 		console.log(this.signupForm);
+	}
+
+	// FormArray extends from AbstractControl, so it is, in essence an array of controls where you can
+	// apply the method push() to add new form controls on the fly
+	onAddHobby(): void {
+		this.getFormArray('hobbies').push(new FormControl(null, Validators.required));
+	}
+
+	// to iterate through the controls properly, we require an array of abstract controls, so we get the form array and fetch
+	// and fetch the controls property, which returns such array to iterate on the *ngFor directive 
+	getControls(path: string) {
+		return this.getFormArray(path).controls;
+	}
+
+	// get a form array
+	// get() returns an AbstractControl originally, but since we are fetching the 'hobbies' array of controls
+	// we explicitly cast this method so it returns the FormArray value
+	private getFormArray(path: string) {
+		return <FormArray> this.signupForm.get(path);
 	}
 
 }
