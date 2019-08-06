3be2d7826115c2aaeefdf49eabbda81abc09cfd4
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2019-08-05 23:38:23 -0600

    Assignment 7 Comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormArray, AbstractControl } from '@angular/forms';
 import { Observable } from 'rxjs';
 
+// reponse of async validator: either null or an object of key strings and boolean values
 type AsyncValidatorResponse = { [key: string]: boolean } | null;
 
 @Component({
@@ -11,10 +12,13 @@ type AsyncValidatorResponse = { [key: string]: boolean } | null;
 })
 export class AppComponent implements OnInit {
 
+	// loading gif url 
 	loadingGifUrl = 'https://upload.wikimedia.org/wikipedia/commons/c/c7/Loading_2.gif';
 
+	// form object
 	projectForm: FormGroup;
 
+	// project statuses
 	projectStatuses: string[] = [
 		'Stable',
 		'Critical',
@@ -23,6 +27,10 @@ export class AppComponent implements OnInit {
 
 	ngOnInit(): void {
 
+		// three fields on form
+		// 1. project name: required synchronous validation and forbiddenProjectName async validation
+		// 2. emial: required and email synchronous validations
+		// 3. projectStatus: initialitize dropdwn on first option (Stable)
 		this.projectForm = new FormGroup({
 			'projectName': new FormControl(null, Validators.required, this.forbiddenProjectName),
 			'email': new FormControl(null, [Validators.required, Validators.email]),
@@ -31,10 +39,12 @@ export class AppComponent implements OnInit {
 
 	}
 
+	// on submit, log info on console
 	onSubmit(): void {
 		console.log(this.projectForm);
 	}
 
+	// async validator: simply check if project name matches hardcoded string to either resolve null or set a custom error code
 	forbiddenProjectName(control: FormControl): Promise<AsyncValidatorResponse> | Observable<AsyncValidatorResponse> {
 		return new Promise<AsyncValidatorResponse>((resolve, reject) => {
 			setTimeout(() => {
