6fde12cf2f3d34b1608a29f2e3b7ebdd521fee35
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2019-08-07 15:28:11 -0600

    Section 17

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,42 @@
-// component parent node selector goes here
-// component class definition goes here
-
 import { Component } from '@angular/core';
 
-// this component will go inside an <app-root> html tag found on the index.html page
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-
-// component class definition
 export class AppComponent {
-
+  servers = [
+    {
+      instanceType: 'medium',
+      name: 'Production Server',
+      status: 'stable',
+      started: new Date(15, 1, 2017)
+    },
+    {
+      instanceType: 'large',
+      name: 'User Database',
+      status: 'stable',
+      started: new Date(15, 1, 2017)
+    },
+    {
+      instanceType: 'small',
+      name: 'Development Server',
+      status: 'offline',
+      started: new Date(15, 1, 2017)
+    },
+    {
+      instanceType: 'small',
+      name: 'Testing Environment Server',
+      status: 'stable',
+      started: new Date(15, 1, 2017)
+    }
+  ];
+  getStatusClasses(server: {instanceType: string, name: string, status: string, started: Date}) {
+    return {
+      'list-group-item-success': server.status === 'stable',
+      'list-group-item-warning': server.status === 'offline',
+      'list-group-item-danger': server.status === 'critical'
+    };
+  }
 }
--- src/app/shared/form-mode.enum.ts
+++
@@ -1,5 +0,0 @@
-// form modes: add mode and update mode
-export enum FormMode {
-	Add,
-	Update
-}
