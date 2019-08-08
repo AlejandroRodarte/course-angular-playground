d85f45150cd351c9a262cd401358c3ed4f453b81
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2019-08-07 22:30:01 -0600

    Lecture 259: Using a Service for Http Requests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
+import { PostsService } from './posts.service';
 
 // interface for the information a post has
 // used to inform TypeScript the data we expect at the end of the http response
@@ -23,104 +22,28 @@ export class AppComponent implements OnInit {
 	// boolean flag to know when are we fetching data to load a loading text or gif
 	isFetching: boolean = false;
 
-	// inject the http client dependency to use client method to make http requests
-    constructor(private http: HttpClient) {
+	// inject the posts service which will handle the http client requests and responses
+    constructor(private postsService: PostsService) {
 
     }
 
+	// on initialization, fetch posts
     ngOnInit() {
-		this.fetchPosts();
+		this.postsService.fetchPosts();
     }
 
 	// on form submission
     onCreatePost(postData: PostProps) {
-
-		// access the http client instance and call the post() method
-		// we pass in the API endpoint, the request body (postData)
-		// and call the subscribe data afterwards to wait for an http response (log it)
-
-		// the '/posts.json' is the API Endpoint in itself (post the data on a /json directory inside Firebase and expect a JSON file))
-		// the post data will be automatically parsed to JSON by Angular
-
-		// http requests are managed by observables so we can subscribe to them and fetch the http response
-		// if we do not set a subscription, Angular implies that we are not interested in a response, so by default
-		// it will not even send the request
-		this.http
-			.post<{ name: string }>(
-				'https://angular-course-app-eeedb.firebaseio.com/posts.json',
-				postData
-			)
-			.subscribe((httpResponse) => {
-				console.log(httpResponse);
-			});
-		  
+		this.postsService.createAndStorePost(postData.title, postData.content);
     }
 
+	// when fetching posts, fetch the posts (duh)
     onFetchPosts() {
-		this.fetchPosts();
+		this.postsService.fetchPosts();
     }
 
     onClearPosts() {
       // Send Http request
-	}
-
-	private fetchPosts() {
-
-		// get request: just place the rest api endpoint url
-		// we desire to edit the raw data that comes from firebase which comes in this format
-
-		// 	{
-		// 		idValue: {
-		// 			title: titleValue
-		// 			content: contentValue
-		// 		},
-		// 		idValue: {
-		// 			title: titleValue
-		// 			content: contentValue
-		// 		}
-		// 	}
-
-		// so we will use observable operators with the pipe() method to beutify this data
-
-		// set the fetching flag to true
-		this.isFetching = true;
-
-		this.http
-			.get<{ [key: string]: PostProps }>(
-				'https://angular-course-app-eeedb.firebaseio.com/posts.json'
-			)
-			.pipe(
-				map((response: { [key: string]: PostProps }) => {
-
-					// we will go from an object of objects to an array of objects
-					const responseArray: PostProps[] = [];
-
-					// loop through each object inside the big object with the keys
-					for (const key in response) {
-
-						// check if it has the key
-						// if so, we will push to the array a brand new object that has the properties of the
-						// looped object plus the key
-						if (response.hasOwnProperty(key)) {
-							responseArray.push( { ...response[key], id: key } );
-						}
-
-					}
-
-					return responseArray;
-
-				})
-			)
-			.subscribe((response) => {
-
-				// get the array of post props and load it into the field
-				this.loadedPosts = response;
-
-				// since the fetching is done, set again the flag to false
-				this.isFetching = false;
-
-			});
-
 	}
 	
 }
---
+++ src/app/posts.service.ts
@@ -0,0 +1,99 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { PostProps } from './app.component';
+import { map } from 'rxjs/operators';
+
+// posts service: global module
+// will handle the http requests
+@Injectable({
+    providedIn: 'root'
+})
+export class PostsService {
+
+    // inject the http client
+    constructor(private http: HttpClient) {
+
+    }
+
+    createAndStorePost(title: string, content: string) {
+        
+        // convert the arguments in a post object
+        const postData: PostProps = {
+            title,
+            content
+        };
+
+        // access the http client instance and call the post() method
+		// we pass in the API endpoint, the request body (postData)
+		// and call the subscribe data afterwards to wait for an http response (log it)
+
+		// the '/posts.json' is the API Endpoint in itself (post the data on a /json directory inside Firebase and expect a JSON file))
+		// the post data will be automatically parsed to JSON by Angular
+
+		// http requests are managed by observables so we can subscribe to them and fetch the http response
+		// if we do not set a subscription, Angular implies that we are not interested in a response, so by default
+        // it will not even send the request
+
+        // execute the post operation
+		this.http
+            .post<{ name: string }>(
+                'https://angular-course-app-eeedb.firebaseio.com/posts.json',
+                postData
+            )
+            .subscribe((httpResponse) => {
+                console.log(httpResponse);
+            });
+
+    }
+
+    fetchPosts() {
+
+        // get request: just place the rest api endpoint url
+		// we desire to edit the raw data that comes from firebase which comes in this format
+
+		// 	{
+		// 		idValue: {
+		// 			title: titleValue
+		// 			content: contentValue
+		// 		},
+		// 		idValue: {
+		// 			title: titleValue
+		// 			content: contentValue
+		// 		}
+		// 	}
+
+		// so we will use observable operators with the pipe() method to beutify this data
+
+		this.http
+			.get<{ [key: string]: PostProps }>(
+				'https://angular-course-app-eeedb.firebaseio.com/posts.json'
+			)
+			.pipe(
+				map((response: { [key: string]: PostProps }) => {
+
+					// we will go from an object of objects to an array of objects
+					const responseArray: PostProps[] = [];
+
+					// loop through each object inside the big object with the keys
+					for (const key in response) {
+
+						// check if it has the key
+						// if so, we will push to the array a brand new object that has the properties of the
+						// looped object plus the key
+						if (response.hasOwnProperty(key)) {
+							responseArray.push( { ...response[key], id: key } );
+						}
+
+					}
+
+					return responseArray;
+
+				})
+			)
+			.subscribe((response) => {
+
+			});
+
+    }
+
+}
