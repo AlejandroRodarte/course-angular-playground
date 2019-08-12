11f1188433318dd219f6f33a98ece5aaac5c8c6d
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2019-08-12 14:31:17 -0600

    Lecture 347: Adding Logic to the Reducer

--- src/app/shopping-list/shopping-list.reducer.ts
+++ src/app/shopping-list/shopping-list.reducer.ts
@@ -1,4 +1,5 @@
 import { Ingredient } from '../shared/ingredient.model';
+import { Action } from '@ngrx/store';
 
 // the initial state is, in most times, a javascript object
 // the shopping list service manages in essence just the array of ingredients
@@ -19,8 +20,21 @@ const initialState = {
 // we set the state argument a default value which will be the initial state
 // the first time this reducer runs, it will load the initial state (kickstarter)
 // on subsequent calls, NgRx will load the previous state
-export function shoppingListReducer(state = initialState, action) {
+export function shoppingListReducer(state = initialState, action: Action) {
 
+    // inside of the reducer: determine the kind of action we receive to know
+    // how to update the state -> we usually use a switch case statement
+    switch (action.type) {
 
+        // action type: add an ingredient; update the state accordingly
+        // never touch the existing state: create a copy of it with the spread operator
+        // attempt to add the new ingredient
+        case 'ADD_INGREDIENT':
+            return {
+                ...state,
+                ingredients: [...state.ingredients, action]
+            };
+
+    }
 
 }
