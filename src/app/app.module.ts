e1631637c715545f7b8d6d3c0a73287bbb9aea6c
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2019-08-12 14:50:49 -0600

    Lecture 349: Setting up the NgRx Store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 // required imports for the app go here
-
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { HttpClientModule } from '@angular/common/http';
-
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
+
+
+// StoreModule: module to create our store
+import { StoreModule } from '@ngrx/store';
+
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+
+
 import { LoggingService } from './logging.service';
+
+
+import { shoppingListReducer } from './shopping-list/store/shopping-list.reducer';
 
 
 // here go the components/modules we will implement on our app
@@ -24,25 +32,20 @@ import { LoggingService } from './logging.service';
 // section 16: added the reactive forms module
 // section 19: added the http client module
 // section 20: added the authentication component
-
 // added the AuthInterceptorService to append the user token on each subsequent request
 // section 21: added the AlertComponent, added the PlaceHolder Directive
-
 // entryComponents: components that should be loaded on runtime (via a factory)
-
 // update: deleted recipes component since they moved to the recipes.module.ts
 // RecipesModule imports RecipesRoutingModule, so we still have access to /recipes child routes
-
 // updated: deleted shopping list components and declared them on the shopping-list.module.ts
 // now using shared module to have access to shared components/directives/modules
-
 // added the core module to notify angular of our root services
 // added now the AuthModule
-
 // imported modules are loaded eagerly
 // since now the RecipesModule is loaded lazily (in a separate coed bundle), we delete it from here
-
 // also deleted eager loading of the AuthModule and ShoppingListModule
+
+// NgRx: added the StoreModule and gave it a map to our reducers
 @NgModule({
 
 	declarations: [
@@ -54,6 +57,9 @@ import { LoggingService } from './logging.service';
 		BrowserModule,
 		AppRoutingModule,
 		HttpClientModule,
+		StoreModule.forRoot({
+			shoppingList: shoppingListReducer
+		}),
 		SharedModule,
 		CoreModule
 	],
--- src/app/shopping-list/store/shopping-list.reducer.ts
+++ src/app/shopping-list/store/shopping-list.reducer.ts
@@ -1,6 +1,8 @@
 import { Ingredient } from '../../shared/ingredient.model';
-import { Action } from '@ngrx/store';
-import { ADD_INGREDIENT } from './shopping-list.actions';
+
+// import everything (*) from the shopping-list.actions
+// and give it the 'ShoppingListActions' alias to access all the imports through it
+import * as ShoppingListActions from './shopping-list.actions';
 
 // the initial state is, in most times, a javascript object
 // the shopping list service manages in essence just the array of ingredients
@@ -21,7 +23,9 @@ const initialState = {
 // we set the state argument a default value which will be the initial state
 // the first time this reducer runs, it will load the initial state (kickstarter)
 // on subsequent calls, NgRx will load the previous state
-export function shoppingListReducer(state = initialState, action: Action) {
+
+// update: make the action argument be of type AddIngredient to know we are expecting a payload
+export function shoppingListReducer(state = initialState, action: ShoppingListActions.AddIngredient) {
 
     // inside of the reducer: determine the kind of action we receive to know
     // how to update the state -> we usually use a switch case statement
@@ -29,12 +33,12 @@ export function shoppingListReducer(state = initialState, action: Action) {
 
         // action type: add an ingredient; update the state accordingly
         // never touch the existing state: create a copy of it with the spread operator
-        // attempt to add the new ingredient
+        // attempt to add the new ingredient with the action payload
         // now we use a constant from the shopping-list.actions.ts to avoid typos and future problems
-        case ADD_INGREDIENT:
+        case ShoppingListActions.ADD_INGREDIENT:
             return {
                 ...state,
-                ingredients: [...state.ingredients, action]
+                ingredients: [...state.ingredients, action.payload]
             };
 
     }
