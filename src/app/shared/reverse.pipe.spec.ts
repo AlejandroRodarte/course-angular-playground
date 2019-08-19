57633deadaa5b5884f527f3a6ec9c01848467c1e
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2019-08-18 20:35:01 -0600

    Lecture 477 Comments

--- src/app/shared/reverse.pipe.spec.ts
+++ src/app/shared/reverse.pipe.spec.ts
@@ -1,7 +1,9 @@
 import {ReversePipe} from './reverse.pipe';
 
+// reverse pipe testing
 describe('ReversePipe', () => {
 
+    // 1. isolated test: check that the pipe works
     it('should reverse the string', () => {
         const reversePipe = new ReversePipe();
         expect(reversePipe.transform('hello')).toEqual('olleh');
